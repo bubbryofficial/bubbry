11f768212db48a5ccd6b3a3b7c47fa587a15dfb6
Bhawuk <bubbryofficial@gmail.com>
2026-02-23 16:42:54 +0530

    fix supabase middleware session

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,21 @@
 import { createServerClient } from "@supabase/ssr";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import type { CookieOptions } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next();
+  let res = NextResponse.next();
 
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
           return req.cookies.get(name)?.value;
         },
-        set(name: string, value: string, options: CookieOptions) {
+        set(name: string, value: string, options: any) {
           res.cookies.set(name, value, options);
         },
-        remove(name: string, options: CookieOptions) {
+        remove(name: string, options: any) {
           res.cookies.set(name, "", options);
         },
       },

0c45783cae9d3a86974cdfe14f639b2e4cca70f1
Bhawuk <bubbryofficial@gmail.com>
2026-02-23 18:40:11 +0530

    fix middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -2,25 +2,20 @@ import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  let res = NextResponse.next({
-    request: {
-      headers: req.headers,
-    },
-  });
+  const res = NextResponse.next();
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
-          return req.cookies.get(name)?.value;
+        getAll() {
+          return req.cookies.getAll();
         },
-        set(name: string, value: string, options: any) {
-          res.cookies.set(name, value, options);
-        },
-        remove(name: string, options: any) {
-          res.cookies.set(name, "", options);
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            res.cookies.set(name, value, options);
+          });
         },
       },
     }
@@ -31,6 +26,7 @@ export async function middleware(req: NextRequest) {
   return res;
 }
 
+// VERY IMPORTANT: this enables middleware everywhere
 export const config = {
   matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };

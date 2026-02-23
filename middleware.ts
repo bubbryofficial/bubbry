364100293f0e3651b91feb4fb81761d6ab8e1fa5
Bhawuk <bubbryofficial@gmail.com>
2026-02-23 14:36:36 +0530

    fix typescript middleware error

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,23 @@
 import { createServerClient } from "@supabase/ssr";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import type { CookieOptions } from "@supabase/ssr";
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
 
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
     {
       cookies: {
-        get(name) {
+        get(name: string) {
           return req.cookies.get(name)?.value;
         },
-        set(name, value, options) {
+        set(name: string, value: string, options: CookieOptions) {
           res.cookies.set(name, value, options);
         },
-        remove(name, options) {
+        remove(name: string, options: CookieOptions) {
           res.cookies.set(name, "", options);
         },
       },
